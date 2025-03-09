eb2532137b24bb082a148783588d760899790f08
memoo96 <mohamedashraf129.ma@gmail.com>
2025-03-09 14:23:33 +0200

    final update

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -71,16 +71,13 @@ export default function Header() {
               />
             </li>
             <li>
+              <a href="">Home</a>
+            </li>
+            <li>
               <a href="">About</a>
             </li>
             <li>
-              <a href="">Articles</a>
-            </li>
-            <li>
               <a href="">Projects</a>
-            </li>
-            <li>
-              <a href="">Speaking</a>
             </li>
             <li>
               <a href="">Contact</a>
